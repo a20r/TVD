60293cc6c6e152f9ecf17134ef8803d47a4127c2
Alex Wallar <aw204@st-andrews.ac.uk>
2015-08-25 09:52:06 -0400

    .gitignore is now working

--- README.md
+++
@@ -1,2 +0,0 @@
-# TopoPRM
-A topologically consistent probabilistic roadmap
--- setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name="topoprm", ext_modules=cythonize("topoprm/*.py"))
